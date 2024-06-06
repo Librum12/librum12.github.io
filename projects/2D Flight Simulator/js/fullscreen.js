24e4b1d3cf570cb08513e3988f63db0d3a23744e
Librum12 <sumrepus@gmail.com>
2024-06-06 08:49:58 +0200

    Add files via upload

---
+++ projects/2D Flight Simulator/js/fullscreen.js
@@ -0,0 +1,10 @@
+var elem = document.getElementById("fullscreenelem");
+function openFullscreen() {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.webkitRequestFullscreen) { 
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { 
+    elem.msRequestFullscreen();
+  }
+}
